28351ddce839b23412c8132699198a7328353073
Виталий <vip72@Air-Vitalij.IGD_Rostelecom>
2024-12-20 19:47:00 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div>
+    <a :href="href">{{ text }}</a>
+   
+  </div>
+</template>
+
+<script>
+export default {
+   data() {
+     return {
+      text: 'page',
+      href: 'pages.html',
+     }
+   }
+}
+</script>
+
+
+
+<style>
+
+</style>
+
+
