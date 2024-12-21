94cb1a7b09297e580484077e8d3d1825e2da2d22
Vitalya Efimov <vitalay.efimov@gmail.com>
2024-12-21 10:19:00 +0500

    Реактивность в фреймворке

--- src/App.vue
+++ src/App.vue
@@ -2,49 +2,35 @@
 export default {
    data() {
      return {
-   
-      //  dey: 0 // Здесь хранится номер дня недели
-     };
+       text: 'xxx'
+     }
    },
    methods: {
-       show: function(nam) {
-         alert('рузультат: ' + nam**2)
-       }
+      change () {
+         this.text = 'yyy'
+      },
+      change2 () {
+         this.text = 'zzz1111'
+      }
 
-    //  deys() {
-    //    const result = this.dayOfTheWeek(this.dey); // Передаем значение dey в качестве аргумента
-    //    return result;
-    //  },
-    //  dayOfTheWeek(num) {
-    //    const days = [
-    //      'Понедельник',
-    //      'Вторник',
-    //      'Среда',
-    //      'Четверг',
-    //      'Пятница',
-    //      'Суббота',
-    //      'Воскресенье'
-    //    ];
-    //    return days[num ]; // Возвращаем день недели по индексу
-    //  }
+
    }
 };
 </script>
 
 <template>
   <div>
-    <!-- <h1 class="text-3xl font-bold underline">{{ cape(text) }}</h1>
-    <p>{{ deys() }}</p> -->
-    <button @click="show(2)">btn1</button>
-    <button @click="show(3)">btn2</button>
+   
+   
+    <button @click="change">Нажми</button>
+    <button @click="change2">Нажми2</button> 
+    <p>{{ text }}</p>
   </div>
 </template>
 
 
 <style scoped>
-.text-3xl{
-  color: rgb(20, 151, 114)
-}
+
 </style>
 
 
