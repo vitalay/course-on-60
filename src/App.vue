66cca7b83ff936be4cb1e7de686110420fcb0aa6
Vitalya Efimov <vitalay.efimov@gmail.com>
2024-12-21 10:08:33 +0500

    Передача параметров в метод

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
-
-<template>
-  <div>
-    <a :href="href">{{ text }}</a>
-   
-  </div>
-</template>
-
 <script>
 export default {
    data() {
      return {
-      text: 'page',
-      href: 'page.html',
-     }
+   
+      //  dey: 0 // Здесь хранится номер дня недели
+     };
+   },
+   methods: {
+       show: function(nam) {
+         alert('рузультат: ' + nam**2)
+       }
+
+    //  deys() {
+    //    const result = this.dayOfTheWeek(this.dey); // Передаем значение dey в качестве аргумента
+    //    return result;
+    //  },
+    //  dayOfTheWeek(num) {
+    //    const days = [
+    //      'Понедельник',
+    //      'Вторник',
+    //      'Среда',
+    //      'Четверг',
+    //      'Пятница',
+    //      'Суббота',
+    //      'Воскресенье'
+    //    ];
+    //    return days[num ]; // Возвращаем день недели по индексу
+    //  }
    }
-}
+};
 </script>
 
+<template>
+  <div>
+    <!-- <h1 class="text-3xl font-bold underline">{{ cape(text) }}</h1>
+    <p>{{ deys() }}</p> -->
+    <button @click="show(2)">btn1</button>
+    <button @click="show(3)">btn2</button>
+  </div>
+</template>
 
 
-<style>
-
+<style scoped>
+.text-3xl{
+  color: rgb(20, 151, 114)
+}
 </style>
 
 
