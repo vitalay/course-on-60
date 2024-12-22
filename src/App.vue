cdab6ab286e2ee64ec8d96e43a8fff7028ddde1c
Vitalya Efimov <vitalay.efimov@gmail.com>
2024-12-22 22:27:07 +0500

    Блокировка элементов в Vue

--- src/App.vue
+++ src/App.vue
@@ -3,33 +3,22 @@ export default {
 
 	data() {
 		return {
-			selectedDay: new Date().getDate(),
-			days: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
-			selectedMonth: new Date().getMonth() + 1,
-			months: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-			selectedYear: new Date().getFullYear(),
-			years: [2020, 2021, 2022, 2023, 2024],
+		isDisabled: false,
 		}
 	},
 
 	methods: {
-	
+	  //   toggleDisabled() {
+		// 	this.isDisabled = !this.isDisabled;
+		// }
 	}
 }
 </script>
 
 <template>
-	<select v-model="selectedDay">
-		<option v-for="day in days">{{ day }}</option>
-	</select>
-	<select v-model="selectedMonth">
-		<option v-for="month in months">{{ month }}</option>
-	</select>
-	<select v-model="selectedYear">
-		<option v-for="year in years">{{ year }}</option>
-	</select>
-
-	<p>{{ selectedDay }} {{ selectedMonth }} {{ selectedYear }}</p>
+	<input type="text" :disabled="isDisabled">
+  <!-- <button  @click="toggleDisabled">click</button> -->
+	<input type="checkbox" v-model="isDisabled">
 
 </template>
 
