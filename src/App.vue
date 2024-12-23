6330341cbe46321de98a2cb1a5f765cafe4c8e93
Vitalya Efimov <vitalay.efimov@gmail.com>
2024-12-23 19:57:02 +0500

    Кнопка для удаления элемента массива в Vue

--- src/App.vue
+++ src/App.vue
@@ -3,29 +3,25 @@ export default {
 
 	data() {
 		return {
-			nevItem: '',
+			
 			items: ['a', 'b', 'c', 'd', 'e']
 			
 		}
 	},
 
 	methods: {
-		add() {
-			this.items.unshift(this.nevItem ) //добавление элемента в начало массива
-			                                  //push - добавление элемента в конец массива
-			                                  //shift -  удаление элемента в начале массива
+		removeItem: function (index) {
+			this.items.splice(index, 1);
 		}
 	}
 }
 </script>
 
 <template>
-
-<input type="text" placeholder="Введите текст" v-model="nevItem">
-	<button @click="add">click</button>
 	<ul>
 		<li v-for="(item, index) in items" :key="index">
 			{{ item }}
+			<button @click="removeItem(index)">Удалить</button>
 		</li>
 	</ul>
 
