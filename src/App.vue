4757a083ebfd7adf9e415827ca0c0d7342b6d5a4
Vitalya Efimov <vitalay.efimov@gmail.com>
2024-12-23 22:39:12 +0500

    Форма для редактирования массива объектов в Vue очень полезная штука

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
-<table>
-	<tr v-for="user in users" :key="user.id">
-		
-		<td>{{ user.name }}</td>
-		<td>{{ user.salary }}</td>
-		<td>{{ user.age }}</td>
-		<td><a @click="removeItem(user.id)" href="#">delete</a></td>
-	</tr>
-</table>
+	<table>
+		<tr v-for="user in users" :key="user.id">
+			<!-- Обычный режим -->
+			<td v-if="!user.isEdit">{{ user.name }}</td>
+			<td v-else><input v-model="user.name" /></td>
+
+			<td v-if="!user.isEdit">{{ user.salary }}</td>
+			<td v-else><input v-model="user.salary" /></td>
+
+			<td v-if="!user.isEdit">{{ user.age }}</td>
+			<td v-else><input v-model="user.age" /></td>
+
+			<!-- Кнопки редактирования и сохранения -->
+			<td>
+				<button v-if="!user.isEdit" @click="edit(user)">Редактировать</button>
+				<button v-else @click="save(user)">Сохранить</button>
+			</td>
+
+			<!-- Кнопка удаления -->
+			<td><button @click="removeItem(user.id)">Удалить</button></td>
+		</tr>
+	</table>
 </template>
 
 <script>
@@ -19,19 +32,22 @@ export default {
 					id: 1,
 					name: 'John',
 					salary: '100',
-					age: '20'
+					age: '20',
+					isEdit: false
 				},
 				{
 					id: 2,
 					name: 'Bob',
 					salary: '200',
-					age: '30'
+					age: '30',
+					isEdit: false
 				},
 				{
 					id: 3,
 					name: 'Alice',
 					salary: '300',
-					age: '40'
+					age: '40',
+					isEdit: false
 				}
 			]
 		};
@@ -40,9 +56,15 @@ export default {
 	methods: {
 		// Метод для удаления элемента по ID
 		removeItem(id) {
-			this.users = this.users.filter(user => {
-				return (user.id !== id);
-			});
+			this.users = this.users.filter(user => user.id !== id);
+		},
+
+		edit(user) {
+			user.isEdit = true;
+		},
+
+		save(user) {
+			user.isEdit = false;
 		}
 	}
 };
@@ -50,12 +72,15 @@ export default {
 
 <style scoped>
 table {
-	border-collapse: collapse
+	border-collapse: collapse;
 }
 
 td {
 	border: 1px solid #ccc;
-	padding: 10px;
+	padding: 10px 30px;
+}
+
+input {
+	width: 70px;
 }
 </style>
-
