740619ac362121a09c59638f50d136b48ad985ad
Vitalya Efimov <vitalay.efimov@gmail.com>
2024-12-24 20:31:48 +0500

    Создание компонентов в цикле в Vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-	<Employe :name="name" :salary="salary" :age="age" />
+	<Employe
+	v-for="user in users" 
+	:key="user.id"
+	:name="user.name"
+	:salary="user.salary"
+	:age="user.age"
+	 />
 
 
 
@@ -13,9 +19,26 @@ export default {
 	},
 	data() {
 		return {
-			name: 'Vladimir',
-			salary: 100,
-			age: 22,
+			users: [
+				{
+					id: 1,
+					name: 'name1',
+					salary: 100,
+					age: 30,
+				},
+				{
+					id: 2,
+					name: 'name2',
+					salary: 200,
+					age: 40,
+				},
+				{
+					id: 3,
+					name: 'name3',
+					salary: 300,
+					age: 50,
+				},
+			],
 		
 		}
 	},
--- src/components/Employe.vue
+++ src/components/Employe.vue
@@ -1,15 +1,20 @@
 <template>
-  имя: {{ name }},
-  <br>
-  зарплата: {{ salary }},
-  <br>
-  года: {{ age }},
-  
+  <table>
+    <tr>
+      <td>{{ name }}</td>
+      <td>{{ age }}</td>
+      <td>{{ salary }}</td>
+    </tr>
+  </table>
+
+
+
 </template>
 
 <script>
 export default {
   props: {
+    key: Number,
     name: String,
     salary: Number,
     age: Number
@@ -25,5 +30,16 @@ export default {
 </script>
 
 <style scoped>
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+tr:nth-child(even){background-color: #f2f2f2;}
 
 </style>
