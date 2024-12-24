0c4aa5b33b3be03dc06f349604e1ac160c542660
Vitalya Efimov <vitalay.efimov@gmail.com>
2024-12-24 21:12:40 +0500

    Однонаправленный поток данных в Vue

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,28 @@
 <template>
-	<Employe
-	v-for="user in users" 
-	:key="user.id"
-	:name="user.name"
-	:salary="user.salary"
-	:age="user.age"
-	 />
-
-
-
+	<User @show="func" @show1="func1"/>
 </template>
 
 <script>
-import Employe from './components/Employe.vue';
+import User from './components/User.vue';
+
+
 export default {
 	components: {
-		Employe
+		User
 	},
 	data() {
 		return {
-			users: [
-				{
-					id: 1,
-					name: 'name1',
-					salary: 100,
-					age: 30,
-				},
-				{
-					id: 2,
-					name: 'name2',
-					salary: 200,
-					age: 40,
-				},
-				{
-					id: 3,
-					name: 'name3',
-					salary: 300,
-					age: 50,
-				},
-			],
-		
+			
 		}
 	},
 
 	methods: {
+		func() {
+			alert('123')
+		},
+		func1() {
+			alert('222')
+		}
 		
 	},
 
---
+++ src/components/User.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="handle">btn</button>
+    <button @click="handle1">btn1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['show', 'show1'],
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+/*************  ✨ Codeium Command ⭐  *************/
+/**
+ * Emits the 'show' event when the button is clicked.
+ */
+
+/******  93c34b5e-4464-49e7-857c-4c8c07227e76  *******/
+    handle() {
+      this.$emit('show')
+    },
+    handle1() {
+      this.$emit('show1')
+    }
+  }
+
+}
+</script>
