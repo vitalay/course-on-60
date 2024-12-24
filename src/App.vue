5a2390b05728383a80fd3b2b315e2f9369d96c0b
Vitalya Efimov <vitalay.efimov@gmail.com>
2024-12-24 22:56:31 +0500

    Реактивное редактирование данных компонента в Vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-	<User @show="func" />
+	<E
+		v-for="employee in employees"
+		:key="employee.id"
+		:name="employee.name"
+		:salary="employee.salary"
+		@remove="del"
+	 />
 </template>
 
 <script>
-import User from './components/User.vue';
+import E from './components/E.vue';
 
 
 export default {
 	components: {
-		User
+		E
 	},
 	data() {
 		return {
-			
+			employees: [
+				{
+					id: 1,
+					name: 'name1',
+					salary: 10000
+				},
+				{
+					id: 2,
+					name: 'name2',
+					salary: 5000
+				},
+				{
+					id: 3,
+					name: 'name3',
+					salary: 7000
+				},
+			],	
+	
 		}
 	},
 
 	methods: {
-
-		func(name, salary) {
-			alert(name + '-' + salary + "$")
-		},
+		del(id) {
+			this.employees = this.employees.filter((employee) => {
+				return employee.id !== id;
+			})
 	
-		
+		}
 	},
 
 };
---
+++ src/components/E.vue
@@ -0,0 +1,39 @@
+<template>
+ <br>
+     {{ name }} 
+     <br>
+     {{ salary }}
+     <br>
+    <button @click="$emit('remove', id)">Удалить</button>
+ <br>
+</template>
+
+<script>
+export default {
+  emits: ['remove'],
+  props: {
+    name: String,
+    salary: Number,
+    id: Number
+  },
+ 
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+
+    // handle() {
+    //   this.$emit('show' , 'Nick', 500)
+    // },
+ 
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
