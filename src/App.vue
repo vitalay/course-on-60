3f537130988969a30aa29528f49bc689eaee1a04
Vitalya Efimov <vitalay.efimov@gmail.com>
2024-12-24 19:39:53 +0500

    Передайте в дочерний компонент массив. Выведите элементы этого массива в виде списка ul.

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,31 @@
 <template>
-	<table>
-		<tr v-for="user in users" :key="user.id">
-			<!-- Обычный режим -->
-			<td v-if="!user.isEdit">{{ user.name }}</td>
-			<td v-else><input v-model="user.name" /></td>
+	<Employe :arg="['machine', 'car', 'piople']" />
+	
 
-			<td v-if="!user.isEdit">{{ user.salary }}</td>
-			<td v-else><input v-model="user.salary" /></td>
 
-			<td v-if="!user.isEdit">{{ user.age }}</td>
-			<td v-else><input v-model="user.age" /></td>
 
-			<!-- Кнопки редактирования и сохранения -->
-			<td>
-				<button v-if="!user.isEdit" @click="edit(user)">Редактировать</button>
-				<button v-else @click="save(user)">Сохранить</button>
-			</td>
-
-			<!-- Кнопка удаления -->
-			<td><button @click="removeItem(user.id)">Удалить</button></td>
-		</tr>
-	</table>
 </template>
 
 <script>
+import Employe from './components/Employe.vue';
 export default {
+	components: {
+		Employe
+	},
 	data() {
 		return {
-			users: [
-				{
-					id: 1,
-					name: 'John',
-					salary: '100',
-					age: '20',
-					isEdit: false
-				},
-				{
-					id: 2,
-					name: 'Bob',
-					salary: '200',
-					age: '30',
-					isEdit: false
-				},
-				{
-					id: 3,
-					name: 'Alice',
-					salary: '300',
-					age: '40',
-					isEdit: false
-				}
-			]
-		};
+		
+		
+		}
 	},
 
 	methods: {
-		// Метод для удаления элемента по ID
-		removeItem(id) {
-			this.users = this.users.filter(user => user.id !== id);
-		},
+		
+	},
 
-		edit(user) {
-			user.isEdit = true;
-		},
-
-		save(user) {
-			user.isEdit = false;
-		}
-	}
 };
 </script>
 
 <style scoped>
-table {
-	border-collapse: collapse;
-}
 
-td {
-	border: 1px solid #ccc;
-	padding: 10px 30px;
-}
-
-input {
-	width: 70px;
-}
 </style>
---
+++ src/components/Employe.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li v-for="arg in items">{{ arg }}</li>
+    
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['arg'],
+    data() {    
+    
+        return {
+          items: this.arg
+
+        
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
